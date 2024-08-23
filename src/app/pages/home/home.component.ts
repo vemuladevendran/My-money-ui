dfdad73dd5a442cda62edc039a994db417af9894
vemuladevendran <devendranvemula@gmail.com>
2024-08-23 00:13:38 -0700

    code clean
    and overflow issue fixed

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { GroupsListComponent } from "../../components/groups-list/groups-list.component";
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
-  imports: [CommonModule, GroupsListComponent],
   standalone: true,
+  imports: [
+    CommonModule,
+    IonicModule,
+    GroupsListComponent
+  ],
 })
-export class HomeComponent  implements OnInit {
+export class HomeComponent implements OnInit {
 
   constructor() { }
 
